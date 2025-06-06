fc96503fad2ee0dded570ee4e06ab52392ac3367
Vladislav Artiukhov <vladislav0art.work@gmail.com>
2025-06-06 22:55:03 +0000

    temp: comment out final process bar update to 100%

--- ydb/public/lib/ydb_cli/import/import.cpp
+++ ydb/public/lib/ydb_cli/import/import.cpp
@@ -859,7 +859,7 @@ TStatus TImportFileClient::TImpl::Import(const TVector<TString>& filePaths, cons
 
     auto finish = TInstant::Now();
     auto duration = finish - start;
-    progressBar.SetProcess(100);
+    // progressBar.SetProcess(100);
     if (duration.SecondsFloat() > 0) {
         std::cerr << "Elapsed: " << std::setprecision(3) << duration.SecondsFloat() << " sec. Total read size: "
             << PrettifyBytes(TotalBytesRead) << ". Average processing speed: "
